9a39b722f5ccade747f82da2f4949572af996408
Amado David Ortiz Gallardo <aortigal@emeal.nttdata.com>
2022-08-31 08:51:21 -0500

    add structure base

---
+++ src/main/java/com/bank/msbootcoinratebackend/MsBootcoinRateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bank.msbootcoinratebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBootcoinRateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBootcoinRateBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/msbootcoinratebackend/MsBootcoinRateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.msbootcoinratebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBootcoinRateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
